f5d8673e363ef2608a9f1b1b8ac2b5235f8ab5a6
Abhishek <abhishek.kaushik16400@gmail.com>
2020-04-15 18:10:27 +0530

    Mo's Algorithm is modified

--- MO's Algorithm.cpp
+++ MO's Algorithm.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
-//#include <ext/pb_ds/assoc_container.hpp> 
-//#include <ext/pb_ds/tree_policy.hpp> 
-//using namespace __gnu_pbds;
-//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-using namespace std;
 #define ll long long
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 ll block;
 struct Query 
 { 
